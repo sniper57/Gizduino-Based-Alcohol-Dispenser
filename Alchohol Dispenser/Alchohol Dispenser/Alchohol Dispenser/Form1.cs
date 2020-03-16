d5cd62487db15e689a3f8efbdf9cfd6bc0d9e3be
John Patrick Galacgac <johnpatrick.galacgac@yahoo.com.ph>
2020-03-17 01:14:33 +0800

    Commit Initial Codes

---
+++ Alchohol Dispenser/Alchohol Dispenser/Alchohol Dispenser/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScaleModelAutomator;
+namespace Alchohol_Dispenser
+{
+    public partial class Form1 : Form
+    {
+        MCUController _mcu = new MCUController();
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _mcu.TriggerOnOff("DISPENSE");
+            _mcu.TriggerOnOff("OK");
+        }
+    }
+}
